7c282c11869cb16f87b1e4780e6f63d46e95ba52
Júlia Souza Magalhães <juliamagalhaes2103@gmail.com>
2025-11-27 09:42:27 -0300

    Att debouncer

--- DEBOUNCER.vhd
+++ DEBOUNCER.vhd
@@ -2,37 +2,37 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- COMPONENTE: DEBOUNCER (elimina bounce do botão físico, realiza o tratamento de ruído)
+-- COMPONENTE: DEBOUNCER (elimina bounce do botÃ£o fÃ­sico, realiza o tratamento de ruÃ­do)
 
 entity debouncer is
-    generic(
-        COUNT_MAX : integer := 2_500_000  -- ~50ms a 50 MHz
-    );
-    port(
-        clk      : in  std_logic;
-        btn_in   : in  std_logic;  -- ativo em LOW
-        btn_stable : out std_logic -- ainda ativo em LOW
-    );
+    generic(
+        COUNT_MAX : integer := 2500000  -- ~50ms a 50 MHz
+    );
+    port(
+        clk       : in  std_logic;
+        btn_in    : in  std_logic;  -- ativo em LOW
+        btn_stable : out std_logic -- ainda ativo em LOW
+    );
 end entity;
 
 architecture rtl_deb of debouncer is
-    signal counter : unsigned(31 downto 0) := (others => '0');
-    signal sync    : std_logic := '1';
+    signal counter : unsigned(31 downto 0) := (others => '0');
+    signal sync    : std_logic := '1';
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if btn_in = sync then
-                counter <= (others => '0');
-            else
-                if counter < COUNT_MAX then
-                    counter <= counter + 1;
-                else
-                    sync <= btn_in;
-                end if;
-            end if;
-        end if;
-    end process;
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if btn_in = sync then
+                counter <= (others => '0');
+            else
+                if counter < COUNT_MAX then
+                    counter <= counter + 1;
+                else
+                    sync <= btn_in;
+                end if;
+            end if;
+        end if;
+    end process;
 
-    btn_stable <= sync;
+    btn_stable <= sync;
 end architecture;
